e04f768825b78b74acb817a5cd6a66eee987952a
arcanelemon <63074135+arcanelemon@users.noreply.github.com>
2020-05-16 18:31:59 -0500

    Main Menu Anims

---
+++ Kenku/Assets/ConfigureData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigureData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
